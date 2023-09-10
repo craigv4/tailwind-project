a725a61e272117f7bbac4d85eca8e7f6dfe8c608
chiragns <chiragns@Lenovo-Y700>
2023-09-11 00:23:01 +0530

    testimonials

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const btn = document.getElementById('menu-btn');
-const nav = document.getElementById('menu');
+// const btn = document.getElementById('menu-btn');
+// const nav = document.getElementById('menu');
 
-btn.addEventListener('click', () => {
-	btn.classList.toggle('open');
-	nav.classList.toggle('flex');
-	nav.classList.toggle('hidden');
-});
+// btn.addEventListener('click', () => {
+// 	btn.classList.toggle('open');
+// 	nav.classList.toggle('flex');
+// 	nav.classList.toggle('hidden');
+// });
 
-console.log('hello');
+// console.log('hello');
